d6dde3056c456e8beafadf058ce9c14c039bdc7e
emptisqzie <lisssr9@gmail.com>
2026-09-20 15:06:40 +0700

    Добавлено распределение ширины

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -44,21 +44,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Row {
-        Text(
-            text = "Large Text\n\nMore Text",
-            Modifier.alignBy(LastBaseline),
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = "Small Text",
-            Modifier.alignByBaseline(),
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-        )
+        TextCell("1", Modifier.weight(weight = 0.2f, fill = true))
+        TextCell("2", Modifier.weight(weight = 0.4f, fill = true))
+        TextCell("3", Modifier.weight(weight = 0.3f, fill = true))
     }
 }
-
 
 
 @Preview(showBackground = true)
